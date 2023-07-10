aa2568de14852d891c53db5278343197dd39d6b7
Muaz <98591478+MuazAshraf@users.noreply.github.com>
2023-07-10 20:16:25 +0500

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,21 +1,31 @@
+from flask import Flask, request, send_file, render_template
 import requests
 import io
 import os
 from PIL import Image
 
+
+app = Flask(__name__)
+
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
 headers = {"Authorization": "Bearer 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"}
+image_path = 'output_image.jpg'
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        prompt = request.form.get('prompt')
+        image_bytes = query(prompt)
+        image = Image.open(io.BytesIO(image_bytes))
+        image_path = 'output_image.jpg'
+        image.save(image_path, 'JPEG')
+        return send_file(image_path, mimetype='image/jpeg')
+    else:
+        return render_template('home.html')
 
 def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
+    response = requests.post(API_URL, headers=headers, json={"inputs": payload})
     return response.content
 
-image_bytes = query({
-    "inputs": "a photo of an Beard Muscled Ape",
-})
-
-# You can access the image with PIL.Image for example
-image = Image.open(io.BytesIO(image_bytes))
-
-# Save the image to a file
-image.save('output_image.jpg', 'JPEG')
+if __name__ == '__main__':
+    app.run(debug=True)
