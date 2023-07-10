dec4fd2648ff530becf6b86d38d4731099175328
Muaz <98591478+MuazAshraf@users.noreply.github.com>
2023-07-10 17:32:30 +0500

    Add files via upload

---
+++ nlp.py
@@ -0,0 +1,23 @@
+import requests
+import io
+import os
+from PIL import Image
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
+
+image_bytes = query({
+    "inputs": "a photo of an Beard Muscled Ape",
+})
+
+# You can access the image with PIL.Image for example
+image = Image.open(io.BytesIO(image_bytes))
+
+# Save the image to a file
+image.save('output_image.jpg', 'JPEG')
+# Open the image
+os.startfile('output_image.jpg', 'JPEG')
